a4631723107b973efa3a1cabfafc15fbc8cbec14
Suman Saurav <saurav@pop-os.localdomain>
2020-09-30 20:41:17 +0530

    added contact form

---
+++ scripts/smtp.js
@@ -0,0 +1,21 @@
+// window.onload = ()=>{
+//     let form = document.getElementById("form")
+
+// form.onsubmit = (e)=>{
+//     e.preventDefault();
+//     console.log(e)
+//     Email.send({
+//         SecureToken : "bd7ca3aa-066b-4980-b02b-b52745733a53",
+//         To : 'suman250299@gmail.com',
+//         From : e.target.elements[1].value,
+//         Subject : e.target.elements[0].value + "From portfolio",
+//         Body : e.target.elements[2].value
+//     }).then(
+//       message => alert(message)
+//     );
+//     // console.log("blyat")
+// }
+
+
+
+// }
